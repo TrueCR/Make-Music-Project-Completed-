025c91f9b438c0eb399a58d9c392f01bf67419b7
TrueCR <31970381+TrueCR@users.noreply.github.com>
2017-10-03 20:49:16 -0600

    Initial commit

---
+++ cs20-write-music-TrueCR-master/script.js
@@ -0,0 +1,16 @@
+function Music() {
+
+    alert("It works"); // Remove this line if you got a popup when you clicked the button. This just verifys that your js is linked.
+    
+    
+    //Break up text into individual sentences in the array "result"
+   
+    
+    // Code in baby steps to ensure it's working
+    
+
+    
+}
+
+
+
